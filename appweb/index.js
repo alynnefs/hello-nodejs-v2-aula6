db3c1a455e58c5f65381d2231ec04fcaba39f01a
alynnefs <alynne@lit.ifce.edu.br>
2016-11-30 19:48:48 -0300

    número

--- appweb/index.js
+++ appweb/index.js
@@ -1,13 +1,21 @@
 // index.js
-const express = require("express");
-const app = express();
+const express    = require("express");
+const bodyParser = require("body-parser");
+const app        = express();
 
 app.use(express.static("public"));
 
-app.get("/dosave", (req,res) => {
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.post("/dosave", (req,res) => {
   console.log("os dados recebidos do formulário são:");
-  console.log(req.query);
-  res.send("<h1>Sucesso!</h1><a href='index.html'>voltar</a>");
+  console.log(req.body);
+  res.send("<h1>Sucesso!</h1><a href='index.html'>voltar </a>");
+});
+
+app.post("/adivinha", (req,res) => {
+  console.log(req.body);
+  res.send("<h1>Sucesso!</h1><a href='numero.html'>voltar </a>");  
 });
 
 app.listen(3000);
